d9bc6dbed9abbc9de448a6783ea7d02a309d19dc
Simone Romano <simoneromano@tutanota.de>
2020-05-09 12:48:34 +0200

    first commit

---
+++ target/debug/deps/coffeed_coffee_service-1f4d76985610c8be.d
@@ -0,0 +1,5 @@
+c:\Users\Sippo\Desktop\git\github\Coffeed-new\coffeed-coffee-service\target\debug\deps\coffeed_coffee_service-1f4d76985610c8be.rmeta: src\main.rs
+
+c:\Users\Sippo\Desktop\git\github\Coffeed-new\coffeed-coffee-service\target\debug\deps\coffeed_coffee_service-1f4d76985610c8be.d: src\main.rs
+
+src\main.rs:
