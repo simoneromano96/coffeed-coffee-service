3e9af9b0911a7ed062a45b8bd65533d2f0426d8e
Simone Romano <simoneromano@tutanota.de>
2020-05-09 12:50:03 +0200

    Clean wrong gitignore problem

--- target/debug/deps/coffeed_coffee_service-bcf281c12cbd1d08.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Sippo\Desktop\git\github\Coffeed-new\coffeed-coffee-service\target\debug\deps\coffeed_coffee_service-bcf281c12cbd1d08.rmeta: src\main.rs
-
-c:\Users\Sippo\Desktop\git\github\Coffeed-new\coffeed-coffee-service\target\debug\deps\coffeed_coffee_service-bcf281c12cbd1d08.d: src\main.rs
-
-src\main.rs:
